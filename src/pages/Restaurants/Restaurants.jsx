56aff86ab049885b8e426a7c305387d5c24070d7
ermcy <ermcy.ermcy@gmail.com>
2025-09-26 17:06:12 +0530

    ball2

--- src/pages/Restaurants/Restaurants.jsx
+++ src/pages/Restaurants/Restaurants.jsx
@@ -16,12 +16,18 @@ const Restaurants = () => {
             </div>
         )
     }
-    let res = undefined;
-    for (const info of shopInfos) {
-        // todo: build the list of restaurants with pretty tiles
-        console.log(info);
-    }
-    return res;
+
+    // todo: use `shopInfos` array to build the tiles
+    // let res = undefined;
+    // for (const info of shopInfos) {
+    //     let img = (<img src={info.iconUrl} alt=""/>);
+    //     console.log(img);
+    //     res += img;
+    //     // res += (<code><pre>{JSON.stringify(info, null, 2)}</pre></code>)
+    //     // todo: build the list of restaurants with pretty tiles
+    //     console.log(info);
+    // }
+    // return res;
 }
 
 export default Restaurants
