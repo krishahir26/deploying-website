f7ca518c7fc9a641026d71cedb9ef96b6a39636e
krishahir26 <ahirkrish787@gmail.com>
2025-09-26 17:05:59 +0530

    changed the ui of home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import "./Header.css"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-contents">
-          <h2>Order your favourite food here</h2>
-          <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest chefs.</p>
-          <button>
-              <Link to="/restaurants">View Available Restaurants</Link>
-          </button>
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes crafted by the finest chefs.
+        </p>
+        <button>
+          <Link to="/restaurants">View Available Restaurants</Link>
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
